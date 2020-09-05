1120424aef3427883060da67e5bd4257d111b704
rakhmedovrs <rakhmedovrs@gmail.com>
2020-09-05 22:25:41 +0300

    Initial commit

---
+++ src/test/java/com/github/rakhmedovrs/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.rakhmedovrs.spring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+}
